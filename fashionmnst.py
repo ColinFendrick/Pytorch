5834816d9d08422f1c3c05bbae5e7096ffe29641
Colin Fendrick <colin.fendrick@gmail.com>
2021-10-15 16:42:47 -0700

    Add in basic fashionmsnt set work

---
+++ fashionmnst.py
@@ -0,0 +1,31 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+BATCH_SIZE = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=BATCH_SIZE)
+test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE)
+
+for X, y in test_dataloader:
+    print("Shape of X [N, C, H, W]: ", X.shape)
+    print("Shape of y: ", y.shape, y.dtype)
+    break

2f0ff56e99737d3f3bf362049c0bbbbc69d58d48
Colin Fendrick <colin.fendrick@gmail.com>
2021-10-15 16:55:34 -0700

    Create a model

--- fashionmnst.py
+++ fashionmnst.py
@@ -1,8 +1,8 @@
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
 
 training_data = datasets.FashionMNIST(
     root="data",
@@ -29,3 +29,28 @@ for X, y in test_dataloader:
     print("Shape of X [N, C, H, W]: ", X.shape)
     print("Shape of y: ", y.shape, y.dtype)
     break
+
+# Get cpu or gpu device for training.
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(DEVICE))
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+MODEL = NeuralNetwork().to(DEVICE)
+print(MODEL)

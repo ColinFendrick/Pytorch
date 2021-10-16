6d3f4f0ec158311f89ca3e8a50d17565a411fc8e
Colin Fendrick <colin.fendrick@gmail.com>
2021-10-16 15:59:20 -0700

    Remove unnceseesary prints

--- fashionmnst.py
+++ fashionmnst.py
@@ -25,11 +25,6 @@ BATCH_SIZE = 64
 train_dataloader = DataLoader(training_data, batch_size=BATCH_SIZE)
 test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE)
 
-for X, y in test_dataloader:
-    print("Shape of X [N, C, H, W]: ", X.shape)
-    print("Shape of y: ", y.shape, y.dtype)
-    break
-
 # Get cpu or gpu device for training.
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(DEVICE))

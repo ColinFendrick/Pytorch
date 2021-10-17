58831df99d3ca4ffcb84531c058aafabeb6c94f9
Colin Fendrick <colin.fendrick@gmail.com>
2021-10-16 17:14:20 -0700

    Generate tensors with torch fns

--- tensors.py
+++ tensors.py
@@ -6,3 +6,15 @@ np_array = np.array(data)
 np_tensor = torch.from_numpy(np_array)
 
 print(np_tensor)
+
+# Shaping tenors
+my_shape = (2,3,)
+
+
+def generate_tensor(generator_fn, shape=my_shape):
+    tensor = generator_fn(shape)
+    print(f"Tensor from {generator_fn.__name__}: {tensor}")
+
+generate_tensor(torch.rand)
+generate_tensor(torch.ones)
+generate_tensor(torch.zeros)

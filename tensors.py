45299818026ae40e3b6006fe7535adc8a224d7f9
Colin Fendrick <colin.fendrick@gmail.com>
2021-10-16 17:15:51 -0700

    Generate tensors with torch fns

--- tensors.py
+++ tensors.py
@@ -14,7 +14,16 @@ my_shape = (2,3,)
 def generate_tensor(generator_fn, shape=my_shape):
     tensor = generator_fn(shape)
     print(f"Tensor from {generator_fn.__name__}: {tensor}")
+    return tensor
 
-generate_tensor(torch.rand)
-generate_tensor(torch.ones)
-generate_tensor(torch.zeros)
+rand = generate_tensor(torch.rand)
+ones = generate_tensor(torch.ones)
+zeros = generate_tensor(torch.zeros)
+
+def get_tensor_attributes(tensor):
+    print(f"Shape of tensor: {tensor.shape}")
+    print(f"Datatype of tensor: {tensor.dtype}")
+    print(f"Device tensor is stored on: {tensor.device}")
+    return tensor
+
+get_tensor_attributes(rand)
